9d2a2526d1b780ca247cf92deb03b4646ad87edc
Samuel <samuel.brendler@gmail.com>
2015-05-06 21:34:52 +0200

    Added FTP Upload and Download Routes

--- src/main/java/at/tuwien/wmpm15/group8/MyRouteBuilder.java
+++ src/main/java/at/tuwien/wmpm15/group8/MyRouteBuilder.java
@@ -1,16 +1,17 @@
 package at.tuwien.wmpm15.group8;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.properties.PropertiesComponent;
 
 /**
  * A Camel Java DSL Router
  */
+
 public class MyRouteBuilder extends RouteBuilder {
 
-    /**
-     * Let's configure the Camel routing rules using Java code...
-     */
     public void configure() {
+
+
 
         // here is a sample which processes the input files
         // (leaving them in place - see the 'noop' flag)
@@ -23,6 +24,29 @@ public class MyRouteBuilder extends RouteBuilder {
                 .otherwise()
                     .log("Other message")
                     .to("file:target/messages/others");
+
+
+
+        // configure properties component for ftp.properties
+        PropertiesComponent pc = getContext().getComponent("properties", PropertiesComponent.class);
+        pc.setLocation("classpath:ftp.properties");
+        /*
+        * Upload files from src/data/attachments to FTP server specified in ftp.properties
+        * ?noop just for testing
+        */
+        from("file:src/data/attachments?noop=true")
+                .log("Uploading file ${file:name}")
+                .to("{{ftp.serverUL}}")
+                .log("Uploaded file ${file:name} complete.");
+
+
+        /*
+        * Download files from FTP server and move them to hidden directory .done
+        */
+        from("{{ftp.serverDL}}")
+            .to("file:target/attachments")
+            .log("Downloaded file ${file:name} complete.");
+
     }
 
 }
