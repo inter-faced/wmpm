ce03854d0be62eff1fb895122cd68b31e72a8a9c
Alexandra <e1327243@student.tuwien.ac.at>
2015-05-10 19:19:51 +0200

    configuration to use a JMS provider:ActiveMQ & update working with FTP

--- src/main/java/at/tuwien/wmpm15/group8/MainApp.java
+++ src/main/java/at/tuwien/wmpm15/group8/MainApp.java
@@ -1,9 +1,16 @@
 package at.tuwien.wmpm15.group8;
 
+import javax.jms.ConnectionFactory;
+
 import at.tuwien.wmpm15.group8.routebuilder.AnExampleRoute;
 import at.tuwien.wmpm15.group8.routebuilder.FtpRoute;
+import at.tuwien.wmpm15.group8.routebuilder.LinkedinRouteBuilder;
 import at.tuwien.wmpm15.group8.routebuilder.TwitterRoute;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
+import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 /**
@@ -17,12 +24,20 @@ public class MainApp {
     public static void main(String... args) throws Exception {
         CamelContext context = new DefaultCamelContext();
 
+        //configure Camel’s JMS component with an appropriate ConnectionFactory
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+        context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+       
         //add all the Routes here
-        context.addRoutes(new AnExampleRoute());
-        context.addRoutes(new TwitterRoute());
+       // context.addRoutes(new AnExampleRoute());
+       // context.addRoutes(new TwitterRoute());
         context.addRoutes(new FtpRoute());
+       // context.addRoutes(new LinkedinRouteBuilder());
 
-        context.start();
+        
+       // Component linkedinComponent = new org.apache.camel.component.linkedin(); 
+
+        		context.start();
     }
 
 }
--- src/main/java/at/tuwien/wmpm15/group8/routebuilder/FtpRoute.java
+++ src/main/java/at/tuwien/wmpm15/group8/routebuilder/FtpRoute.java
@@ -1,32 +1,44 @@
 package at.tuwien.wmpm15.group8.routebuilder;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.properties.PropertiesComponent;
 
 public class FtpRoute extends RouteBuilder {
 
+	
     public void configure() throws Exception {
 
+    	//Polling for FTP messages (FTP endpoint) and sending them to the incomingOrders queue (JMS endpoint)
+    	from("ftp://workflow2015.square7.ch:21/applications?username=XXX&password=YYY").process(new Processor() { 
+    		public void process(Exchange exchange) throws Exception {
+    			System.out.println("We just downloaded: " + exchange.getIn().getHeader("CamelFileName"));
+    			}
+    		}).
+    	to("jms:incomingDocuments");
+       
+    	/*
         // configure properties component for ftp.properties
         PropertiesComponent pc = getContext().getComponent("properties", PropertiesComponent.class);
         pc.setLocation("classpath:ftp.properties");
 
-        /*
+        
         * Upload files from src/data/attachments to FTP server specified in ftp.properties
         * ?noop just for testing
-        */
+        
             from("file:src/data/attachments?noop=true")
                     .log("Uploading file ${file:name}")
                     .to("{{ftp.serverUL}}")
                     .log("Uploaded file ${file:name} complete.");
 
-        /*
+        
         * Download files from FTP server and move them to hidden directory .done
-        */
+        
             from("{{ftp.serverDL}}")
                     .to("file:target/attachments")
                     .log("Downloaded file ${file:name} complete.");
+         */
     }
-
 
 }
