993ba764f727fc5f6ca8216e31f0401a897fcbd0
samira <samiramiss@github.com>
2015-05-15 16:51:11 +0200

    adding new dependency. add a utility class to read from properties file

--- src/main/java/at/tuwien/wmpm15/group8/beans/MongoDbBean.java
+++ src/main/java/at/tuwien/wmpm15/group8/beans/MongoDbBean.java
@@ -6,6 +6,8 @@ import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Properties;
 
+import at.tuwien.wmpm15.group8.utils.CredentialsReader;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
@@ -17,15 +19,11 @@ public class MongoDbBean  {
 	
 	public MongoDbBean()
 	{
-		Properties prop= getMongoDbProperty();
+		Properties prop= CredentialsReader.read();
 		MongoCredential credential = MongoCredential.createMongoCRCredential(prop.getProperty("mongodb.userName"), prop.getProperty("mongodb.dbName"), prop.getProperty("mongodb.password").toCharArray());
 		
 		
-			try {
-				mongoClient = new MongoClient(new ServerAddress(prop.getProperty("mongodb.url"),Integer.parseInt(prop.getProperty("mongodb.port"))), Arrays.asList(credential));
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			}
+			mongoClient = new MongoClient(new ServerAddress(prop.getProperty("mongodb.url"),Integer.parseInt(prop.getProperty("mongodb.port"))), Arrays.asList(credential));
 		
 
 		
@@ -38,39 +36,5 @@ public class MongoDbBean  {
 	}
 
 
-	private Properties getMongoDbProperty()
-	{
-		Properties prop = new Properties();
-		InputStream input = null;
 
-		try {
-
-			String filename = "credentials.properties";
-			input = MongoDbBean.class.getClassLoader().getResourceAsStream(filename);
-			if(input==null){
-				System.out.println("Sorry, unable to find " + filename);
-				return null;
-			}
-
-			prop.load(input);
-
-
-			System.out.println("dbName: "+ prop.getProperty("mongodb.dbName"));
-			System.out.println("userName: "+prop.getProperty("mongodb.userName"));
-			System.out.println("password: "+prop.getProperty("mongodb.password"));
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally{
-			if(input!=null){
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-			} 
-		}
-		return prop;
-	}
 }
---
+++ src/main/java/at/tuwien/wmpm15/group8/utils/CredentialsReader.java
@@ -0,0 +1,48 @@
+package at.tuwien.wmpm15.group8.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import at.tuwien.wmpm15.group8.beans.MongoDbBean;
+
+public class CredentialsReader {
+	
+	
+	public static Properties read()
+	{
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+
+			String filename = "credentials.properties";
+			input = MongoDbBean.class.getClassLoader().getResourceAsStream(filename);
+			if(input==null){
+				System.out.println("Sorry, unable to find " + filename);
+				return null;
+			}
+
+			prop.load(input);
+
+
+			System.out.println("dbName: "+ prop.getProperty("mongodb.dbName"));
+			System.out.println("userName: "+prop.getProperty("mongodb.userName"));
+			System.out.println("password: "+prop.getProperty("mongodb.password"));
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally{
+			if(input!=null){
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			} 
+		}
+		return prop;
+	}
+
+}
