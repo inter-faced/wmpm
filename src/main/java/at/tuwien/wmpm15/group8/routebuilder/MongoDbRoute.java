41c2309a80d29a5d8b2223abf9e6727197626754
Samuel <samuel.brendler@gmail.com>
2015-06-16 12:51:15 +0200

    Added Invite and Reject Functionality (MailQueue)

--- src/main/java/at/tuwien/wmpm15/group8/routebuilder/MongoDbRoute.java
+++ src/main/java/at/tuwien/wmpm15/group8/routebuilder/MongoDbRoute.java
@@ -1,15 +1,13 @@
 package at.tuwien.wmpm15.group8.routebuilder;
 
+import at.tuwien.wmpm15.group8.beans.ProcessCriteria;
 import org.apache.camel.Exchange;
-import org.apache.camel.ExchangePattern;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.properties.PropertiesComponent;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
-import at.tuwien.wmpm15.group8.beans.ProcessCriteria;
 
 public class MongoDbRoute  extends RouteBuilder {
 
@@ -24,10 +22,10 @@ public class MongoDbRoute  extends RouteBuilder {
 		from("mongodb:myDb?database={{mongodb.webdbName}}&collection={{mongodb.webdbApplicantsCollectionCapped}}&tailTrackIncreasingField=increasing&cursorRegenerationDelay=60000ms")
 		.id("tailableCursorConsumer1")
 		// .autoStartup(false)
-		.to("direct:inputApplicants" );
-		
-		
-		from("direct:inputApplicants" )
+				.to("direct:inputApplicants");
+
+
+		from("direct:inputApplicants")
 		.process(new Processor() {
 			@Override
 			public void process(Exchange exchange) throws Exception {
@@ -57,7 +55,7 @@ public class MongoDbRoute  extends RouteBuilder {
 		.log("Applicant qualified!")
 
 		.otherwise()
-		.to("jms:queue:email.queue")
+				.to("direct:consumer")
 		.log("Applicant not qualified!");
 
 
