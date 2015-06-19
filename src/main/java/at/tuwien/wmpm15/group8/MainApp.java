0dec1017e0b6b399a6fb1af14d1b5e4259c3190a
samira <samiramiss@github.com>
2015-06-19 13:43:09 +0200

    refactor main

--- src/main/java/at/tuwien/wmpm15/group8/MainApp.java
+++ src/main/java/at/tuwien/wmpm15/group8/MainApp.java
@@ -22,21 +22,42 @@ public class MainApp {
      */
     public static void main(String... args) throws Exception {
 
-        AppSubmissionSimulator simulator= new AppSubmissionSimulator();
-        simulator.startSimulation();
-        
-        JndiContext jndiContext = new JndiContext();
+   
+       
+        CamelContext context = initiateCamelContext();
+
+        prepareSimulation(context);
+
+        addRoutes(context);
+
+        startSimulation(context);
+
+
+      
+
+    }
+
+    private static CamelContext initiateCamelContext() throws Exception
+    {
+    	/*
+        SimpleRegistry registry = new SimpleRegistry();
         MongoDbBean mgBean=new MongoDbBean();
-        jndiContext.bind("myDb", mgBean.getConnection());
+        registry.put("myDb", mgBean.getConnection());
+        CamelContext  context = new DefaultCamelContext(registry);
+    	 */
+    	 JndiContext jndiContext = new JndiContext();
+         MongoDbBean mgBean=new MongoDbBean();
+         jndiContext.bind("myDb", mgBean.getConnection());
 
-        CamelContext context = new DefaultCamelContext(jndiContext);
-        context.getProperties().put(Exchange.LOG_DEBUG_BODY_MAX_CHARS, "3000");
-        context.getShutdownStrategy().setTimeout(3); // for twitter messages that should not be processed
-
-        // Set up the ActiveMQ JMS Components
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-        context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-
+         CamelContext context = new DefaultCamelContext(jndiContext);
+         context.getProperties().put(Exchange.LOG_DEBUG_BODY_MAX_CHARS, "3000");
+         context.getShutdownStrategy().setTimeout(3); // for twitter messages that should not be processed
+    	
+         return context;
+    }
+    
+    private static void addRoutes( CamelContext context) throws Exception
+    {
         //add all the Routes here
         context.addRoutes(new MulticastRoute());
         context.addRoutes(new TwitterRoute());
@@ -54,16 +75,27 @@ public class MainApp {
         context.addRoutes(new AggregatorRoute());
         context.addRoutes(new FacebookRoute());
          */
-
-        context.start();
-        
-        context.startRoute("tailableCursorConsumer");
-        
-        Thread.sleep(70000);
-
-        context.stopRoute("tailableCursorConsumer");
-      
-
     }
-
+    
+    private static void startSimulation( CamelContext context) throws Exception
+    {
+        context.start();       
+        context.startRoute("tailableCursorConsumer");        
+        Thread.sleep(70000);
+        context.stopRoute("tailableCursorConsumer");
+    	
+    }
+    
+    
+    private static void prepareSimulation( CamelContext context) throws Exception
+    {
+        // Set up the ActiveMQ JMS Components
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+        context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        
+        AppSubmissionSimulator simulator= new AppSubmissionSimulator();
+        simulator.startSimulation();
+    	
+    }
+    
 }
